cfaa64d95d9e3891d7491e08b4aba29727cf0e50
rmara <rmara@RobertoPC>
2025-01-29 20:07:13 -0600

    added for loop

--- src/Demo1.java
+++ src/Demo1.java
@@ -2,10 +2,15 @@
 public class Demo1 {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("******");
-		
+
+		for (int i = 0; i < 10; i++) {
+
+			System.out.println(i);
+
+		}
+
 	}
-	
-	
+
 }
